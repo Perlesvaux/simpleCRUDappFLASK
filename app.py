54887085908f6d3815132def09b0e7a4738d85e3
Perlesvaux Le Gallois <99514791+Perlesvaux@users.noreply.github.com>
2022-03-18 17:30:26 -0600

    this repo contains a simple CRUD app. DB is SQLite

---
+++ app.py
@@ -0,0 +1,103 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+#SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<task {self.id}>'
+
+
+#homepage.
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue LOL'
+
+            #return redirect('/')#f"{request.form['content']} es un pipian careverga"
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks = tasks)
+        #return render_template('tetris.html')
+
+#delete button.
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'Oops! Sorry fam ¯\_(ツ)_/¯'
+
+
+#upddate button.
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task_to_update = Todo.query.get_or_404(id)
+    if request.method == 'POST':
+        task_to_update.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'LOL Sorry fam, wrong input!'
+
+    else:
+        return render_template('update.html', task = task_to_update)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+#URL:jdbc:mysql://localhost:3306/primero
+#mysql://username:password@server/db
+#mysql://admin:admin@primero/db
+
+
+
+
+
+# py app.py
+#
